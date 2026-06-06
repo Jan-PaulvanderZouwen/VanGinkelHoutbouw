f7b492d5f3e200b0d2f8b50e21081974a0a23a14
Jan-PaulvanderZouwen <jan-paul@pr3nt.nl>
2026-06-06 11:50:05 +0200

    Add default page template

---
+++ wp-content/themes/vanginkelhoutbouw/page.php
@@ -0,0 +1,14 @@
+<?php
+defined('ABSPATH') || exit;
+get_header();
+?>
+<section class="section section--page">
+    <div class="container narrow content-flow">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <p class="eyebrow">Van Ginkel Houtbouw</p>
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
+    </div>
+</section>
+<?php get_footer(); ?>
