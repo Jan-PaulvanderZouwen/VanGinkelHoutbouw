12b46f57e454ec918dd5dfe5fc21155e6342964b
Jan-PaulvanderZouwen <jan-paul@pr3nt.nl>
2026-06-06 11:47:14 +0200

    Add asset enqueue logic

---
+++ wp-content/themes/vanginkelhoutbouw/inc/enqueue.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Asset loading.
+ *
+ * @package vanginkelhoutbouw
+ */
+
+defined('ABSPATH') || exit;
+
+add_action('wp_enqueue_scripts', function (): void {
+    $css_files = [
+        'vgh-base'       => '/assets/css/base.css',
+        'vgh-layout'     => '/assets/css/layout.css',
+        'vgh-components' => '/assets/css/components.css',
+        'vgh-templates'  => '/assets/css/templates.css',
+    ];
+
+    foreach ($css_files as $handle => $path) {
+        wp_enqueue_style($handle, VGH_THEME_URI . $path, [], VGH_THEME_VERSION);
+    }
+
+    wp_enqueue_script('vgh-navigation', VGH_THEME_URI . '/assets/js/navigation.js', [], VGH_THEME_VERSION, true);
+    wp_enqueue_script('vgh-main', VGH_THEME_URI . '/assets/js/main.js', [], VGH_THEME_VERSION, true);
+
+    if (is_page_template('page-templates/template-home.php') || is_page_template('page-templates/template-configurator.php')) {
+        wp_enqueue_script('three', 'https://unpkg.com/three@0.165.0/build/three.min.js', [], '0.165.0', true);
+        wp_enqueue_script('vgh-three-hero', VGH_THEME_URI . '/assets/js/three-hero.js', ['three'], VGH_THEME_VERSION, true);
+    }
+
+    if (is_page_template('page-templates/template-configurator.php')) {
+        wp_enqueue_script('vgh-configurator-preview', VGH_THEME_URI . '/assets/js/configurator-preview.js', ['vgh-three-hero'], VGH_THEME_VERSION, true);
+    }
+});
